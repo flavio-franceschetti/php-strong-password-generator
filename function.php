1bce0d50c367a6d3ebed90beefb8dbd9d8e8604f
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-08-08 22:42:05 +0200

    feat[bonus]: add other options for password generator

--- function.php
+++ function.php
@@ -1,12 +1,26 @@
 <?php 
 
 // funzione per generare la password random prendendo come parametri una lunghezza e una lista di caratteri
-function generateRandomPassword($psw_length, $charList){
+function generateRandomPassword($psw_length, $charList, $characters_index){
   // variabile per innserire la password generata
   $randomPsw = '';
+
+  // controllo che la lunghezza dei caratteri da estrarre non sia più lunga della lista selezionata dei caratteri in $characters_array
+  $length = 0;
+  // eseguo un ciclo forEach sugli indici per recuperare la lunghezza totale di ogni indice tramite strlen
+  foreach($characters_index as $charIndex){
+    // uso l'operatore += perché stiamo usando numeri solo numeri qundi non si usa il "."
+    $length += strlen($charList[$charIndex]);
+  }
+  //creo una condizione per la quale se la lunghezza della password inserita dall'utente è maggiore della stringa selezionata nell'array la lunghezza inserita dall'utente diventa automaticamente la lunghezza massima della stringa dell'array
+  if($psw_length > $length){
+    $psw_length = $length;
+  }
+
+
   //ciclo basato sulla lunghezza della password che deve essere inserita dall'utente
   while(strlen($randomPsw) < $psw_length){
-    $character = getRandomChar($charList);
+    $character = getRandomChar($charList, $characters_index);
     //aggiungo la condizione se allowrepetitions è true o se $randomPsw contiene $character allora aggiungi la lettera a $randomPsw
     if($_GET['allowRepetitions'] || !str_contains($randomPsw, $character)){
       $randomPsw .= $character;
@@ -17,6 +31,12 @@ function generateRandomPassword($psw_length, $charList){
   }
 
 
-  function getRandomChar($charList){
-    return $charList[rand(0, strlen($charList) - 1)];
+  function getRandomChar($charList, $characters_index){
+    //estraggo l'indice randomico da cui prendere il carattere nell'array $characters_array
+    $array_index = $characters_index[rand(0, count($characters_index) - 1)];
+    //attraverso l'indice seleziono la stringa dalla quale prendere il carattere
+    $charString = $charList[$array_index];
+
+    // ritorna un numero casuale fra 0 alla lunghezza della stringa di caratteri selezionata
+    return $charString[rand(0, strlen($charString) - 1)];
   }
--- index.php
+++ index.php
@@ -4,13 +4,16 @@ include __DIR__ . '/partials/head.php';
 
 // array dei vari caratteri per generare le password
 $characters_array = [
-'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ', 
-'0123456789',
-'!?&%$<>^+-*/()[]{}@#_='
+    'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ', 
+    '0123456789',
+    '!?&%$<>^+-*/()[]{}@#_='
 ];
 
 //array con tutti i caratteri insieme
-$all_characters = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!?&%$<>^+-*/()[]{}@#_=';
+// $all_characters = 'abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!?&%$<>^+-*/()[]{}@#_=';
+
+//se vengono inseriti gli dindici tramite le chackbox del form allora viene salvato il valore e usato quello altrimenti vengono usati tutti gli idici che sono 0,1,2 perche in $characters_array abbiamo 3 indici da poter ciclare per estrapolare i caratteri
+$characters_index = $_GET['char'] ?? [0,1,2];
 
 //se la lunghezza è stata inserita
 if(isset($_GET['pswlength']) && !empty($_GET['pswlength'])) {
@@ -19,7 +22,7 @@ if(isset($_GET['pswlength']) && !empty($_GET['pswlength'])) {
         //apre la sessione
         session_start();
         // genero la password
-        $response = generateRandomPassword($_GET['pswlength'], $all_characters);
+        $response = generateRandomPassword($_GET['pswlength'], $characters_array, $characters_index);
         // scrivo la psw nella sessione
         $_SESSION['response'] = $response;
         //reindirizzo alla pagina per mostrare la password
@@ -36,7 +39,7 @@ if(isset($_GET['pswlength']) && !empty($_GET['pswlength'])) {
 ?>
 
 <!-- body dell'html -->
-<body class="d-flex align-items-center justify-content-center bg-light">
+<body class="d-flex align-items-center justify-content-center bg-dark">
     <div class="container">
         <!-- titolo e sottotitolo -->
         <div class="text-center my-5">
@@ -46,14 +49,15 @@ if(isset($_GET['pswlength']) && !empty($_GET['pswlength'])) {
     
         <!-- testo che cambia quando viene visualizzata la psw generata -->
         <div class="text-white p-3 bg-success"> <?php echo $output ?></div>
-      
+      <!-- form  -->
         <form action="index.php" method="GET" class="bg-light p-3 my-4">
             <div class="mb-3">
                 <span>Lunghezza password:</span>
                 <input type="number" name="pswlength" value="pswlength"><br>
             </div>
 
-            <div class="d-flex gap-4">
+            <!-- radio consenti ripetizioni -->
+            <div class="d-flex gap-4 mb-3">
                 <span>Consenti ripetizioni: </span>
                 <div class="form-check">
                     <input class="form-check-input" type="radio" name="allowRepetitions" value="1" id="flexRadioDefault1" checked>
@@ -69,9 +73,29 @@ if(isset($_GET['pswlength']) && !empty($_GET['pswlength'])) {
                 </div>
             </div>
             
+            <!-- checkbox solo lettere solo numeri solo simboli -->
+            <div class="form-check">
+                <!-- in name viene inserito come un array per usarlo come tale poi per inserire l'indice dei caratteri selezionato -->
+                <input class="form-check-input" type="checkbox" value="0" name="char[]" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                    Solo lettere
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="1" name="char[]" id="flexCheckChecked" >
+                <label class="form-check-label" for="flexCheckChecked">
+                    Solo numeri
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="2" name="char[]" id="flexCheckChecked" >
+                <label class="form-check-label" for="flexCheckChecked">
+                    Solo simboli
+                </label>
+            </div>
             
 
-            <div class="mt-3">
+            <div class="mt-4">
                 <button type="submit" class="btn btn-primary">Invia</button>
                 <button type="reset" class="btn btn-secondary">Annulla</button>
             </div>
